ebc5a42da934437f3e331ed97b3bbdb26e36dc36
Aarthy M <aarthimanoharan2003@gmail.com>
2023-04-10 09:29:29 +0000

    Error handling

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,8 +1,23 @@
+pragma solidity ^0.8.0;
 
-// SPDX-License-Identifier: MIT
-// compiler version must be greater than or equal to 0.8.17 and less than 0.9.0
-pragma solidity ^0.8.17;
-
-contract HelloWorld {
-    string public greet = "Hello World!";
+contract RequireAssertRevert {
+    
+    function withdraw(uint amount) public {
+        require(amount <= address(this).balance, "Not enough balance."); // check if contract has enough balance
+        msg.sender.transfer(amount); // send requested amount to the sender
+    }
+    
+    function divide(uint numerator, uint denominator) public pure returns (uint) {
+        require(denominator != 0, "Cannot divide by zero."); // check if denominator is not zero
+        return numerator / denominator;
+    }
+    
+    function doSomething(bool flag) public {
+        if (flag) {
+            assert(2 + 2 == 5); // will cause the transaction to revert as the assertion is false
+        } else {
+            revert("Something went wrong."); // will revert the transaction with a custom error message
+        }
+    }
+    
 }
